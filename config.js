e3d7c9dd7c178ad37e53cc465ebb47511c3b6362
realsoftdev <final.fantasy.dev@gmail.com>
2017-03-24 07:25:14 -0400

    final commit

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = [
+    {
+        "url" : "https://newyork.craigslist.org/search/mnh/bbb",
+        "note" : "all services in manhatten"
+    },
+    {
+        "url" : "https://newjersey.craigslist.org/search/cms",
+        "note" : "cellular in new jersey"
+    }
+]
